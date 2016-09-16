9b8761719e2a6636e495d4e366e77753744406cd
Jingyu Zhao <jingyuz@usc.edu>
2016-09-16 12:26:14 -0700

    3D drawing

---
+++ lab2/Source/MeshComponent.cpp
@@ -0,0 +1,9 @@
+#include "MeshComponent.h"
+#include "Game.h"
+
+IMPL_COMPONENT(MeshComponent, DrawComponent);
+
+void DrawComponent::Draw(class Renderer& render)
+{
+
+}
---
+++ lab2/Source/MeshComponent.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "DrawComponent.h"
+#include "Mesh.h"
+
+class MeshComponent : public DrawComponent
+{
+	DECL_COMPONENT(MeshComponent, DrawComponent);
+public:
+	void Draw(class Renderer& render) override;
+	void SetMesh(MeshPtr mesh) { mMesh = mesh; }
+	MeshPtr GetMesh() { return mMesh; }
+private:
+	MeshPtr mMesh;
+};
+
+DECL_PTR(MeshComponent);
